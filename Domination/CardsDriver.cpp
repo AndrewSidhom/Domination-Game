464fd54024ef57dbcc96718bc7b284b391f2cfab
mcontreras <mcw396@hotmail.com>
2019-10-11 18:09:01 -0400

    Added a second hand to testing

--- Domination/CardsDriver.cpp
+++ Domination/CardsDriver.cpp
@@ -16,12 +16,15 @@ using namespace std;
 int main() {
 
 	//Create deck and hand object
-	Deck d(10);
+	Deck d(14);
 	Hand h1;
+	Hand h2;
 	map<int, int> ownedCountries;
+	map<int, int> ownedCountries2;
+
 
 	//Make country data
-	ownedCountries[1]= 3;
+	ownedCountries[1] = 3;
 	ownedCountries[2] = 4;
 	ownedCountries[3] = 6;
 	ownedCountries[4] = 2;
@@ -29,33 +32,58 @@ int main() {
 	ownedCountries[6] = 1;
 	ownedCountries[7] = 8;
 
+	ownedCountries2[8] = 3;
+	ownedCountries2[9] = 4;
+	ownedCountries2[10] = 6;
+	ownedCountries2[11] = 2;
+	ownedCountries2[12] = 5;
+	ownedCountries2[13] = 1;
+	ownedCountries2[14] = 8;
+
+
+
+	//Draw all cards from deck and show hand
+	while (!d.isEmpty()) {
+		h1.addCardToHand(d.draw());
+		h2.addCardToHand(d.draw());
+	}
+	cout << "Hand 1:\n--------" << endl;
+	h1.showHand();
+
+	cout << "\nHand 2:\n--------" << endl;
+	h2.showHand();
+
+	//Show distribution
+	cout << "\nInfantry count: " << h1.armies[0] + h2.armies[0] << endl;
+	cout << "Artillery count: " << h1.armies[1] + h2.armies[1] << endl;
+	cout << "Cavalry count: " << h1.armies[2] + h2.armies[2] << endl;
+
+	cout << "\nTesting mandatory exchange on hand 1..." << endl;
 	cout << "Owned Countries Before Exchange:\n----------------" << endl;
 	for (int i = 1; i < 8; i++) {
 		cout << "Country " << i << ": " << ownedCountries[i] << " armies" << endl;
 	}
 
-	//Draw all cards from deck and show hand
-	while (!d.isEmpty()) {
-		h1.addCardToHand(d.draw());
-	}
-	h1.showHand();
 
-	//Show distribution
-	cout << "\nInfantry count for h1: " << h1.armies[0] << endl;
-	cout << "Artillery count for h1: " << h1.armies[1] << endl;
-	cout << "Cavalry count for h1: " << h1.armies[2] << endl;
-
-	cout << "\nTesting exchange..." << endl;
 	h1.exchange(&ownedCountries, &d, true);
-	
+
 	cout << "Owned Countries After Exchange:\n----------------" << endl;
 	for (int i = 1; i < 8; i++) {
 		cout << "Country " << i << ": " << ownedCountries[i] << " armies" << endl;
 	}
-	h1.showHand();
 
-	cout << "\nTest cancelling exchange..." << endl;
-	h1.exchange(&ownedCountries, &d, false);
+	cout << "\nTesting optional exchange on hand 2..." << endl;
+	cout << "Owned Countries Before Exchange:\n----------------" << endl;
+	for (int i = 8; i < 15; i++) {
+		cout << "Country " << i << ": " << ownedCountries2[i] << " armies" << endl;
+	}
+
+	h2.exchange(&ownedCountries2, &d, false);
+
+	cout << "Owned Countries After Exchange:\n----------------" << endl;
+	for (int i = 8; i < 15; i++) {
+		cout << "Country " << i << ": " << ownedCountries2[i] << " armies" << endl;
+	}
 
 	system("pause");
 	return 0;
